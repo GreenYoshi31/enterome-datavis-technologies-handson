cc46bb56051d001aade7ae9cf75c7cb5dde7da10
GreenYoshi31 <rweigel13@gmail.com>
2023-03-22 23:57:20 +0100

    Exercise 1 - 8

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,1 +1,1 @@
-<div style="color: rgb(2, 96, 45)">My name is <span style="font-weight: bold; color: rgb(96, 2, 45)">Rico</span>.</div>
+<div style="color: rgb(1, 96, 45)">My name is <span style="font-weight: bold; color: rgb(96, 2, 45)">Rico</span>.</div>
